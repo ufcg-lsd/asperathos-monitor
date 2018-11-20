0bf249e33bb3b1d5f4f5cb64f5d0c8852d45fa62
Javan Lacerda <javan.lacerda@ccc.ufcg.edu.br>
2018-11-20 16:47:36 -0300

    add tests to kubejobs plugin

---
+++ monitor/tests/mocks/mock_monasca.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2017 UFCG-LSD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class MockMonascaConnector():
+
+    def __init__(self):
+
+        self.metrics = {}
+
+    
+    def send_metrics(self, metrics):
+
+        try:
+            self.metrics[metrics[0]['name']].append(metrics)
+
+        except Exception as ex:
+
+            self.metrics[metrics[0]['name']] = []
+            self.metrics[metrics[0]['name']].append(metrics)
